289db066ec4251bacf1c142073180d299e2ee007
Juanu12 <juanferumana@hotmail.com>
2025-05-27 08:23:31 -0500

    feat: :sparkles: improving code

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,35 +1,55 @@
 const Food = document.getElementById("Diaplayfood")
+const Beef = document.getElementById("DiaplayBeef")
 
-
- async function Fetchdata(){
-    try{
+async function Fetchdata() {
+    try {
         const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php")
 
-        if(!response.ok){
-            throw new Error ( `Error HTTP ${response.status} `)
-        }        
+        if (!response.ok) {
+            throw new Error(`Error HTTP ${response.status} `)
+        }
 
 
         const data = await response.json()
         console.log(data)
         return data.meals[0]
 
-    } catch(error)
-    {
-        console.error( "Ocurrió un error", error)
+    } catch (error) {
+        console.error("Ocurrió un error", error)
     }
 
 
 }
 
+async function Fetchdata2() {
+    try {
+        const response = await fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef")
+
+        if (!response.ok) {
+            throw new Error(`Error HTTP ${response.status} `)
+        }
 
 
-async function Displaydata(){
+        const data = await response.json()
+        console.log(data)
+        return data
+
+
+    } catch (error) {
+        console.error("Ocurrió un error", error)
+    }
+
+
+}
+
+Fetchdata2()
+
+async function Displaydata() {
     let info = ""
-    for(i=0; i<10; i++){
+    for (i = 0; i < 10; i++) {
 
         let comida = await Fetchdata()
-    console.log(comida)
+        console.log(comida)
         info += `   <div class="Food_container">
         <img src="${comida.strMealThumb}" alt="">
        <p>${comida.strMeal}</p>
@@ -52,3 +72,54 @@ Displaydata()
 
 
 
+document.getElementById("Filtrar").addEventListener("change", async function (e) {
+
+    e.preventDefault()
+
+
+    const Opt = document.getElementById("Filtrar").value
+    if (Opt === "Beef") {
+
+        try {
+            const response = await fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef")
+
+            if (!response.ok) {
+                throw new Error(`Error HTTP ${response.status} `)
+            }
+
+
+            const data = await response.json()
+            console.log(data)
+            Displaybeef(data.meals)
+        } catch (error) {
+            console.error("Ocurrió un error", error)
+        }
+
+
+        async function Displaybeef(meals) {
+            let info2 = ""
+            for (i = 0; i < meals.length; i++) {
+                const meal = meals[i]
+                info2 += `   <div class="Food_container">
+                <img src="${meal.strMealThumb}" alt="">
+               <p>${meal.strMeal}</p>
+               <p>${meal.idMeal}</p>
+               
+                <button>Mas información</button>
+                 </div>`
+
+
+
+            }
+            Beef.innerHTML = info2
+        }
+
+
+
+
+
+    }
+}
+
+
+)
